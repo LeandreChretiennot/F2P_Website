1ecbf753ad3e0e9608cedc9b6f88d1c579f8639c
leandre <l.chretiennot@student.isartdigital.com>
2025-03-18 16:43:08 +0100

    [UPDATE] change url again

--- src/Components/Home/Account/Main.jsx
+++ src/Components/Home/Account/Main.jsx
@@ -30,8 +30,8 @@ export default function Main(props) {
                 <div id="account_infos" className="flex_col">
                     <div id="account_settings">
                         <p>Name</p>
-                        <button onClick={() => Test("https://ftpapiacces.altsproduction.ovh/databasesapi/API/read.php?ext=utilisateurs")}>Account settings</button>
-                        <button onClick={() => Test("https://cors-anywhere.herokuapp.com/https://ftpapiacces.altsproduction.ovh/databasesapi/API/read.php?ext=utilisateurs")}>CORS anywhere</button>
+                        <button onClick={() => Test("https://ftpapiacces.altsproduction.ovh/databasesapi/API/read.php?ext=utilisateurs")}>utilisateurs</button>
+                        <button onClick={() => Test("https://ftpapiacces.altsproduction.ovh/databasesapi/index.html")}>Index.html</button>
                     </div>
                     <div id="account_expenses" className="scrollbox"></div>
                 </div>
